2611991041f081a04ad9135e209d0c70add009e9
Frank Schmitt <frankschmitt@gmx.de>
2023-11-06 22:47:20 +0100

    no-streaming: add greetings endpoint with count parameter

--- demo-no-streaming/src/main/java/de/qwhon/springboot/demonostreaming/GreetingController.java
+++ demo-no-streaming/src/main/java/de/qwhon/springboot/demonostreaming/GreetingController.java
@@ -1,5 +1,7 @@
 package de.qwhon.springboot.demonostreaming;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,5 +17,14 @@ public class GreetingController {
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/greetings")
+    public List<Greeting> greetings(@RequestParam(value="count", defaultValue = "1")Long count) {
+        List<Greeting> result = new ArrayList<Greeting>();
+        for(Long i=0l; i<count; ++i) {
+            result.add(new Greeting(counter.incrementAndGet(), String.format(template, i.toString())));
+        }
+        return result;
     }
 }
